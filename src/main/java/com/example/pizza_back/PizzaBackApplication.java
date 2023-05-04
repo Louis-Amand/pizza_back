141a0179b8f8f2117c4405cd179a0a134a9f53d5
Louis-Amand <48965957+Louis-Amand@users.noreply.github.com>
2023-05-04 09:26:18 +0200

    initial commit

---
+++ src/main/java/com/example/pizza_back/PizzaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizza_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pizza_back/PizzaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizza_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
